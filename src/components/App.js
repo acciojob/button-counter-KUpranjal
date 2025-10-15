bf940b5e84a10c41f00b31a076c441698135920c
Pranjal <pranjal7903@email.com>
2025-10-15 19:30:01 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [count , setCount]=useState(1)
+  const [count , setCount]=useState(0)
   return (
     <div>
       <p>Button clicked {count} times</p>
